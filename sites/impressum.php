4480516a7b4179165676265da36cb1f8f859bf3d
AlphRaue <alph-raue@alph-raue.de>
2018-09-25 13:55:28 +0200

    Formular ist fertig. Erforderliche Felder definiert (alle). Prüfung auf Gültigkeit der EMail Adresse fertig. Bei Fehlern kann nicht gesendet werden. Der HEAD Bereich wurde ausgelagert und nun auf allen Seiten includiert. Favicon eingebaut. Erforderliche Kommentare in wichtigen Bereichen gesetzt.

--- sites/impressum.php
+++ sites/impressum.php
@@ -1,22 +1,14 @@
 <!DOCTYPE html>
 <html lang="de">
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <title>Connputer Impressum</title>
-    <link href="../css/style.css" rel="stylesheet">
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i"
-          rel="stylesheet">
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
-    <link href="../css/animate.css" rel="stylesheet">
-    <script src="../js/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/0.1.12/wow.min.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
+    <?php include_once("../sites/static/head.html"); ?>
 </head>
-<body>
-<?php include_once( "../sites/static/header.html" ); ?>
 
+<body>
+<?php include_once("../sites/static/header.html"); ?>
+
+<!-- MAIN impressum.php -->
 <div class="container-fluid aktu-bg">
     <div class="container">
         <h1 class="heading-container">impressum</h1>
@@ -54,13 +46,9 @@
             </div>
         </div>
     </div>
-	<?php include_once( "../sites/static/footer.html" ); ?>
+    <!-- MAIN End -->
 
-</div>
-</div>
-</div>
-
-<?php include_once( "../sites/static/footer.html" ); ?>
+    <?php include_once("../sites/static/footer.html"); ?>
 
 </body>
 </html>
